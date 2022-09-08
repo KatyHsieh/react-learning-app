36fb171250eb083e7c38b57d048269a1d4a9db02
Katy <cat0420cat@gmail.com>
2022-09-08 14:34:13 +0800

    home components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,5 @@
 import React from "react";
 
-const button = document.querySelector(".shiny");
-const readout = document.querySelector("p");
-button.addEventListener("mousemove", (e) => {
-  const { x, y } = button.getBoundingClientRect();
-  button.style.setProperty("--x", e.clientX - x);
-  button.style.setProperty("--y", e.clientY - y);
-});
-
 function Home() {
   return (
     <main>
